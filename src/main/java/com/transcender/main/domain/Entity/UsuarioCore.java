fa6baa973f867a04c456033d6a1a3e9681053ba7
Wagratom <wagratom111@gmail.com>
2025-07-30 20:50:12 -0300

    add regras de negocio no domain

--- src/main/java/com/transcender/main/domain/Entity/UsuarioCore.java
+++ src/main/java/com/transcender/main/domain/Entity/UsuarioCore.java
@@ -1,5 +1,7 @@
 package com.transcender.main.domain.Entity;
 
+import com.transcender.main.domain.exceptions.UsuarioArgumentInvalid;
+
 import java.time.Instant;
 
 public class UsuarioCore {
@@ -15,8 +17,11 @@ public class UsuarioCore {
 
     public UsuarioCore() {}
 
-    public UsuarioCore(Long id, String email, String senha, String nickname, String telefone,
-                       boolean online, Instant criadoEm, Instant atualizadoEm) {
+    UsuarioCore(Long id, String email, String senha, String nickname, String telefone,
+                boolean online, Instant criadoEm, Instant atualizadoEm) {
+        validateUsuario(email, nickname, senha);
+        if (id == null || id <= 0) throw new UsuarioArgumentInvalid("Id invalido");
+        
         this.id = id;
         this.email = email;
         this.senha = senha;
@@ -28,12 +33,49 @@ public class UsuarioCore {
         this.atualizadoEm = atualizadoEm;
     }
 
+    public UsuarioCore(String email, String senha, String nickname, String telefone, boolean online) {
+        this.email = email;
+        this.senha = senha;
+        this.nickname = nickname;
+        this.telefone = telefone;
+        this.online = online;
+        this.criadoEm = Instant.now();
+        this.atualizadoEm = Instant.now();
+    }
+
+    public void validateUsuario(String email, String nickname, String senha) {
+        validateLogin(email, nickname, senha);
+
+        if (nickname != null && nickname.length() > 15) {
+            throw new UsuarioArgumentInvalid(String.format("Nickname inválido '%s' maior que 15 caracteres", nickname));
+        }
+
+        if (email != null && email.length() > 50) {
+            throw new UsuarioArgumentInvalid("Email inválido: deve ter no máximo 100 caracteres");
+        }
+    }
+
+    public void validateLogin(String email, String nickname, String senha) {
+        boolean emailVazio = email == null || email.trim().isEmpty();
+        boolean nicknameVazio = nickname == null || nickname.trim().isEmpty();
+
+        if (emailVazio && nicknameVazio) {
+            throw new UsuarioArgumentInvalid("Email ou nickname devem ser especificados");
+        }
+
+        if (senha == null || senha.trim().isEmpty()) {
+            throw new UsuarioArgumentInvalid("Senha inválida");
+        }
+    }
+
+
     // Getters
     public Long getId() { return id; }
     public String getEmail() { return email; }
     public String getSenha() { return senha; }
     public String getNickname() { return nickname; }
     public String getTelefone() { return telefone; }
+    public String getIdf() { return (email != null && !email.trim().isEmpty()) ? email : nickname; }
     public boolean getOnline() { return online; }
     public boolean getAtive() { return ative; }
     public Instant getCriadoEm() { return criadoEm; }
@@ -47,6 +89,6 @@ public class UsuarioCore {
     public void setTelefone(String telefone) { this.telefone = telefone; }
     public void setOnline(boolean online) { this.online = online; }
     public void setAtive(boolean ative) { this.ative = ative; }
-    public void setCriadoEm(Instant criadoEm) { this.criadoEm = criadoEm; }
+    private void setCriadoEm(Instant criadoEm) { this.criadoEm = criadoEm; }
     public void setAtualizadoEm(Instant atualizadoEm) { this.atualizadoEm = atualizadoEm; }
 }
