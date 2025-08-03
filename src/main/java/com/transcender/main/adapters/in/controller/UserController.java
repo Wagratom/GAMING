e657cbdde2e1d047ac513bdfe64d62129643ab2a
Wagratom <wagratom111@gmail.com>
2025-08-03 00:49:58 -0300

    fix:  comendando rota de update e corrigindo controller routes

--- src/main/java/com/transcender/main/adapters/in/controller/UserController.java
+++ src/main/java/com/transcender/main/adapters/in/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -21,6 +22,10 @@ public class UserController {
         this.userApplication = userApplication;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Map<String, Object>>> getAllUsers() {
+        return ResponseEntity.ok().body(userApplication.getUsersOnlines());
+    }
     //POST -> /users
     @PostMapping
     public ResponseEntity<Map<String, Object>> registerUser(@Valid @RequestBody UserDtoRegister body) {
@@ -35,30 +40,30 @@ public class UserController {
     }
 
     //GET -> users/1234
-    @GetMapping
+    @GetMapping("/{userId}")
     public ResponseEntity<Map<String, Object>> getUserById(@PathVariable Long userId) {
-        UserCore user =  userApplication.getUserById(userId);
-        return UserDtoRegister.toEntity(user);
+        return UserDtoRegister.toEntity(userApplication.getUserById(userId));
     }
 
     //DELETE -> users/1234
-    @DeleteMapping
+    @DeleteMapping("/{userId}")
     public ResponseEntity<String> deleteUserById(@PathVariable Long userId) {
         userApplication.deleteUser(userId);
         return ResponseEntity.ok().body("Sucesso");
     }
 
     //put -> /users
-    @PutMapping
-    public void UpdateUser(@Valid @RequestBody UserDtoUpdate body) {
-        UserCore user = new UserCore(
-                body.getEmail(),
-                body.getPassword(),
-                body.getNickname(),
-                body.getTelefone(),
-                Optional.of(body.getId())
-        );
+    //@PutMapping
+    //public ResponseEntity<String, Object> UpdateUser(@Valid @RequestBody UserDtoUpdate body) {
+    //    UserCore user = new UserCore(
+    //            body.getEmail(),
+    //            body.getPassword(),
+    //            body.getNickname(),
+    //            body.getTelefone(),
+    //            Optional.of(body.getId())
+    //    );
 
-        userApplication.registerUser(user);
-    }
+    //    userApplication.updateUser(user);
+    //    ResponseEntity.ok().
+    //}
 }
