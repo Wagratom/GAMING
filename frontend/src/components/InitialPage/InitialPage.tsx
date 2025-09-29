a5853c07545b2220a900f6c8afc436d5532e6379
Wagratom <wagratom111@gmail.com>
2025-09-29 11:23:28 -0300

    refact initial page

--- frontend/src/components/InitialPage/InitialPage.tsx
+++ frontend/src/components/InitialPage/InitialPage.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { UserData, UserDto } from './Contexts/Contexts';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function InicialPage() {
 
@@ -25,51 +26,7 @@ export default function InicialPage() {
 
 	const retryCount = useRef(0);
 	const timeoutId = useRef<NodeJS.Timeout | null>(null);
-
 	const navigate = useNavigate();
-	// Cria a conexão socket e retorna a instância
-
-	// Função para buscar dados do usuário com retry e timeout
-	function getInfoUser(timeForNewRequestAxios: number) {
-		axios.get(`${process.env.REACT_APP_API_URL}/users/me`, {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem("token")}`
-			},
-			withCredentials: true
-		})
-			.then(async (res) => {
-				if (res.status !== 200) {
-					throw new Error('Erro ao buscar dados do usuário');
-				}
-
-				const data: UserDto = await res.data;
-				updateDataUser(data);
-
-				//reiniciando variaveis de controle
-				retryCount.current = 0;
-				if (timeoutId.current) {
-					clearTimeout(timeoutId.current);
-					timeoutId.current = null;
-				}
-			})
-			.catch((err) => {
-				console.log("err.response: ", err.response?.status)
-				if (err.response?.status === 401) {
-					alert("Sessão expirada ou não autorizada. Por favor, faça login novamente.");
-					navigate('/login')
-				}
-				retryCount.current++;
-				if (retryCount.current >= 5) {
-					alert('O servidor está indisponível no momento, tente novamente mais tarde.');
-					retryCount.current = 0;
-					timeForNewRequestAxios = 60000;
-				}
-
-				// Limpa timeout anterior para evitar múltiplos
-				if (timeoutId.current) clearTimeout(timeoutId.current);
-				timeoutId.current = setTimeout(() => getInfoUser(timeForNewRequestAxios), timeForNewRequestAxios);
-			});
-	}
 
 	useEffect(() => {
 		const token: string | null = localStorage.getItem("token");
@@ -79,13 +36,72 @@ export default function InicialPage() {
 			return;
 		}
 
+		const disconnect = () => {
+			const route = `${process.env.REACT_APP_API_URL}/logout`;
+
+			axios.patch(route, {}, {
+				headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+				withCredentials: true,
+			})
+				.then((res) => {
+					if (res.status === 200) {
+						Cookies.remove("jwtToken");
+						navigate("/");
+					}
+				})
+				.catch((err) => {
+					console.error("Erro ao desconectar:", err);
+				});
+		}
+
+
+		function getInfoUser(timeForNewRequestAxios: number) {
+			axios.get(`${process.env.REACT_APP_API_URL}/users/me`, {
+				headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+				withCredentials: true
+			})
+				.then(async (res) => {
+					if (res.status !== 200) {
+						throw new Error('Erro ao buscar dados do usuário');
+					}
+
+					const data: UserDto = await res.data;
+					updateDataUser(data);
+
+					//reiniciando variaveis de controle
+					retryCount.current = 0;
+					if (timeoutId.current) {
+						clearTimeout(timeoutId.current);
+						timeoutId.current = null;
+					}
+				})
+				.catch((err) => {
+					if (err.response?.status === 401) {
+						alert("Sessão expirada ou não autorizada. Por favor, faça login novamente.");
+						navigate('/login')
+					}
+					retryCount.current++;
+					if (retryCount.current >= 5) {
+						alert('O servidor está indisponível no momento, tente novamente mais tarde.');
+						retryCount.current = 0;
+						timeForNewRequestAxios = 60000;
+					}
+
+					// Limpa timeout anterior para evitar múltiplos
+					if (timeoutId.current) clearTimeout(timeoutId.current);
+					timeoutId.current = setTimeout(() => getInfoUser(timeForNewRequestAxios), timeForNewRequestAxios);
+				});
+		}
+
 		getInfoUser(10000);
 
 		// Cleanup quando componente desmonta: limpa timeout e desconecta socket
 		return () => {
 			if (timeoutId.current) clearTimeout(timeoutId.current);
-		};
-	}, []);
+			disconnect()
+		}
+
+	}, [navigate]);
 
 	return (
 		<UserData.Provider value={{ user: user, updateDataUser: updateDataUser }}>
